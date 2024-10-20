9c9003c17c80cb621b97416a8e3b206eecc3c37c
maheshbsl <miggybsl@gmail.com>
2024-10-20 14:02:24 +0700

    signature verification

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -1,20 +1,30 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 
-pragma solidity ^0.8.26 ;
+pragma solidity ^0.8.26;
 
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract MerkleAirdrop {
+contract MerkleAirdrop is EIP712 {
     using SafeERC20 for IERC20;
 
     error MerkleAirdrop_InvalidProof();
     error MerkleAirDrop_AlreadyClaimed(address account);
+    error MerkleAirdrop_InavlidSignature();
 
     event Claimed(address account, uint256 amount);
 
     address[] claimers;
     mapping(address claimer => bool claim) private s_hasClaimed;
+
+    struct  AirdropClaim {
+        address account;
+        uint256 amount;
+    }
+
+    bytes32 public constant MESSAGE_TYPEHASH = keccak256("AirdropClaim(address account,uint256 amount)");
 
     // some list of addresses
     // allow someone in the list to claim token
@@ -25,45 +35,65 @@ contract MerkleAirdrop {
     /**
      * When a new MerkleAirdrop is created,
      * we have to pass the merkle root and airdrop token address in the constructor.
-     **@param merkleRoot The merkle root
+     * @param merkleRoot The merkle root
      * @param airdropToken The airdopTokn
-     * */
-    constructor (bytes32 merkleRoot, IERC20 airdropToken) {
+     *
+     */
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) EIP712("MerkleAirdrop", "1.0") {
         i_merkleRoot = merkleRoot;
         i_airdropToken = airdropToken;
     }
-
     /**
-     * 
+     *
      * @param account The account the user wants to claim the token in
      * @param amount The token amount that the user wants to claim
      * @param merkeleProof This includes hashes of the sibling nodes in the merkle tree
+     * @param v The signature v
+     * @param r The signature r
+     * @param s The signature s
      */
 
-    function claim(address account, uint256 amount, bytes32[] calldata merkeleProof) external {
-
+    function claim(address account, uint256 amount, bytes32[] calldata merkeleProof, uint8 v, bytes32 r, bytes32 s) external {
         // if the account has already claimed then revert
         if (s_hasClaimed[account]) {
             revert MerkleAirDrop_AlreadyClaimed(account);
         }
-         // leaf -> take address and amount and hash it 
-         bytes32 leaf = keccak256(abi.encode(account, amount));
+        //if the signature is invalid then revert
+        if (!_isValidSignature(account, getMessageHash(account, amount), v, r, s)) {
+            revert MerkleAirdrop_InavlidSignature();
+        }
+        // leaf -> take address and amount and hash it
+        bytes32 leaf = keccak256(abi.encode(account, amount));
 
-         // proof 
-         // if the proof is invalid then revert
+        // proof
+        // if the proof is invalid then revert
         if (!MerkleProof.verify(merkeleProof, i_merkleRoot, leaf)) {
             revert MerkleAirdrop_InvalidProof();
         }
-        // update the mapping for claim account 
+        // update the mapping for claim account
         s_hasClaimed[account] = true;
-        
+
         // emit the event as claimed
         emit Claimed(account, amount);
-        
+
         // if the proof is valid then claim
         i_airdropToken.safeTransfer(account, amount);
     }
+    /**
+     * @param account The address of the account that is claiming the airdrop
+     * @param amount The amount of the token that the user wants to claim
+     * @return the digest (combine the domain separator and hash message, add the eip191 prefix, hash the result)
+     */
+    function getMessageHash(address account, uint256 amount) public view returns (bytes32) {
+        return _hashTypedDataV4(
+            keccak256(abi.encode(MESSAGE_TYPEHASH, AirdropClaim({account: account, amount: amount})))
+        );
+    }
 
+    function _isValidSignature(address account, bytes32 digest, uint8 v, bytes32 r, bytes32 s) public pure returns (bool) {
+        (address recoveredAddress,,) = ECDSA.tryRecover(digest, v, r, s);
+        return recoveredAddress == account;
+    }
 
     function getAirDropToken() public view returns (IERC20) {
         return i_airdropToken;
@@ -72,9 +102,8 @@ contract MerkleAirdrop {
     function getMerkleRoot() public view returns (bytes32) {
         return i_merkleRoot;
     }
-    
+
     function getClaimerStutus(address account) public view returns (bool) {
         return s_hasClaimed[account];
     }
-
 }
--- test/MerkleAirdropTest.t.sol
+++ test/MerkleAirdropTest.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.26;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {MerkleAirdrop} from "src/MerkleAirdrop.sol";
 import {BagelToken} from "src/BagelToken.sol";
 import {DeployMerkleAirdrop} from "script/DeployMerkleAirdrop.s.sol";
@@ -15,6 +15,12 @@ contract MerkleAirdropTest is Test, ZkSyncChainChecker {
     bytes32 public constant ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
     uint256 public constant AMOUNT_TO_CLAIM = 25 * 10**18; // a user can claim 25 bagel
     uint256 public s_amountToTransfer = 4 * 25 * 10**18;
+    bytes32 proof1 = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 proof2 = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] public proof = [proof1, proof2];
+    address gasPayer;
+    address user;
+    uint256 privateKey;
 
     function setUp() public {
         if (!isZkSyncChain()) {
@@ -29,7 +35,29 @@ contract MerkleAirdropTest is Test, ZkSyncChainChecker {
             // transfer the tokens to the airdrop
             token.transfer(address(airdrop), s_amountToTransfer);
         }
+        (user, privateKey) = makeAddrAndKey("user");
+         gasPayer = makeAddr("gasPayer");
     }
 
-    function testuserCanClaim() public {}
+    function testanyoneCanClaimUsingSignature() public {
+        // staring balance
+        uint256 startingBalnce = token.balanceOf(user);
+        // gettig the message hash
+        bytes32 digest = airdrop.getMessageHash(user, AMOUNT_TO_CLAIM);
+
+        // sign the message with the user's private key
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, digest);
+
+        // claim the airdrop using the user signature by the gasPayer
+        vm.prank(gasPayer);
+        airdrop.claim(user, AMOUNT_TO_CLAIM, proof, v, r, s);
+
+        // balance of the user after claim
+        uint256 balanceAfterClaim = token.balanceOf(user);
+        assert(balanceAfterClaim == startingBalnce + AMOUNT_TO_CLAIM);
+    }
+
+    function testUserCanClaim() public  view{
+       console.log("user address: ", user);
+    }
 }
